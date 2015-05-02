7094f5eb96d1d0787418f1b8d966ffbec61e2f44
TheRabbitologist <albatross@nosuch.net>
2015-05-02 12:39:34 -0700

    Upgraded the debug header.

--- src/dgdebug.hpp
+++ src/dgdebug.hpp
@@ -3,14 +3,16 @@
 
 //For those of us who can't use a debugger with Qt Creator.
 
-#include <iostream>
 #include <cassert>
+
+#include <QDebug>
 
 #undef NDEBUG
 
-#define DEBUG_EMIT(a) std::cerr << a << std::endl;
-#define DEBUG_EMIT_TEST(a,b) if(a) {std::cerr << b << std::endl;}
-#define DEBUG_ASSERT(a) assert(a);
-#define DEBUG_ASSERT_TEST(a,b) if(a) assert(b);
+#define DEBUG_EMIT(val) qDebug() << val << '\n';
+#define DEBUG_EMIT_TEST(cond,val) if(cond) {qDebug() << val << '\n';}
+#define DEBUG_EMIT_LIST(list) for(const auto& val : list) {qDebug() << '\t' << val << '\n';}
+#define DEBUG_ASSERT(test) assert(test);
+#define DEBUG_ASSERT_TEST(cond,test) if(cond) assert(test);
 
 #endif // DGDEBUG_HPP
