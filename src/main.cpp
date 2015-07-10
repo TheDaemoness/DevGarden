a4f760c0ecf9816426ee01ae382030e61b747590
TheRabbitologist <albatross@nosuch.net>
2015-07-10 09:07:39 -0700

    DevGarden's window name is now set to DG_NAME.

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@
 int main(int argc, char **argv) {
 
 	QApplication a(argc, argv);
-	a.setApplicationName("DevGarden");
+	a.setApplicationName(DG_NAME);
 	a.setWindowIcon(QIcon("://icon.png"));
 
 	// Translation
