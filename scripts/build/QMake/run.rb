a77dc51359d00d4d41c0253c6d918947e8cb1653
TheRabbitologist <admin@rabbitology.net>
2015-09-27 22:12:39 -0700

    Script now has the correct build path.

--- scripts/build/QMake/run.rb
+++ scripts/build/QMake/run.rb
@@ -5,7 +5,7 @@ require 'pathname'
 include RbConfig
 
 _path = Pathname.new(ARGV[0])
-_buildpath = _path.dirname.to_s+"/../build-"+(_path.dirname.basename.to_s);
+_buildpath = _path.dirname.to_s+"/../"+(_path.dirname.basename.to_s)+"-build";
 
 if !Dir.exist? _buildpath
 	Dir.mkdir _buildpath
