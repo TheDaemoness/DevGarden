978e8531f3c66be7a0a7e5b9ee8bef50e91ad096
TheRabbitologist <admin@rabbitology.net>
2015-09-28 20:40:57 -0700

    Further work on Target.

--- src/build/target.cpp
+++ src/build/target.cpp
@@ -1,7 +1,19 @@
 #include "target.h"
+#include "../langregistry.h"
 
-Target::Target()
-{
-
+Target::Target(const LangRegistry& lr, const QFileInfo& fi, const QString& tar) : file(fi), target(tar) {
+	buildsys = lr.getLang(fi.fileName(),false);
 }
 
+QString Target::rm(const QString& key) {
+	auto it = vars.find(key);
+	if(it != vars.end()) {
+		QString retval = it->second;
+		vars.erase(it);
+		return retval;
+	}
+}
+
+bool Target::build() const {
+	lr.
+}
--- src/build/target.h
+++ src/build/target.h
@@ -1,10 +1,20 @@
 #ifndef TARGET_H
 #define TARGET_H
 
+#include <QFileInfo>
+#include <map>
 
 class Target {
+	QFileInfo file;
+	QString target, buildsys;
+	std::map<QString,QString> vars;
 public:
-	Target();
+	Target(const LangRegistry& lr, const QFileInfo& fi, const QString& target = "");
+
+	QString& set(const QString& key) {return vars[key];}
+	QString rm(const QString& key);
+
+	bool build() const;
 };
 
 #endif // TARGET_H
